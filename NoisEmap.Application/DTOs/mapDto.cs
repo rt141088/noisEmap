159e07b33b187eba54c293baf67515cec3bd69c4
rt141088 <rm560955@fiap.com.br>
2025-11-08 19:57:15 -0300

    README ATUALIZADO 08 11

--- NoisEmap.Application/DTOs/mapDto.cs
+++ NoisEmap.Application/DTOs/mapDto.cs
@@ -1,10 +1,11 @@
-﻿namespace NoisEmap.Application.DTOs
+﻿
+public class MapDto
 {
-    public class MapDto
-    {
-        public int Id { get; set; }
-        public string Location { get; set; } = string.Empty;
-        public double NoiseLevel { get; set; }
-        public DateTime RecordedAt { get; set; }
-    }
+    public int Id { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public double NoiseLevel { get; set; }
+    public DateTime RecordedAt { get; set; }
+
+    // NOVO: Coleção de links
+    public List<LinkDto> Links { get; set; } = new List<LinkDto>();
 }
